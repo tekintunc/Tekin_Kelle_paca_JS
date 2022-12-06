2415cfefcfa614bc934810c86282593825b91917
tekintunc <115110134+tekintunc@users.noreply.github.com>
2022-12-06 20:46:31 +0300

    kodlar değişti

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-let animals = ["tavuk","inek","koyun","domuz"];
-
-let result;
-
-result = animals.shift(); 
-result = animals.pop(); 
-
-console.log(animals.length * 4 );
+// burada bütün çiftlikteki hayvanlar array ile sıralanır
+let animals = ["tavuk", "inek", "tavuk", "inek", "koyun", "domuz"];
+let inek = 0;
+let tavuk = 0;
+let domuz = 0;
+let koyun = 0;
 
 
+for (let index = 0; index < animals.length; index++) {
+  // her eleman için tek tek baksın
+  let animallist = animals[index];
+  if (animallist.includes("tavuk")) {
+    tavuk++;
+  } else if (animallist.includes("inek")) {
+    inek++;
+  } else if (animallist.includes("domuz")) {
+    domuz++;
+  } else if (animallist.includes("koyun")) {
+    koyun++;
+  }
+}
+
+console.log("kellepaçaya uygun ayak sayısı: " + (inek + koyun) * 4);
